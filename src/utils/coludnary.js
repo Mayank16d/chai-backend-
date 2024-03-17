0b4b73073753e7bc30086aaa170d1927fdff0b24
Mayank Nandwani <nandwanimayank007@gmail.com>
2024-03-17 13:10:36 +0530

    Add user registration controller with avatar and cover image upload

--- src/utils/coludnary.js
+++ src/utils/coludnary.js
@@ -25,7 +25,7 @@ cloudinary.config({
     }
   }
 
-
-  cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
-  { public_id: "olympic_flag" }, 
-  function(error, result) {console.log(result); });
+export {uploadOnCloudinary}
+//   cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+//   { public_id: "olympic_flag" }, 
+//   function(error, result) {console.log(result); });
