01648bd77c9c5a65f35a4a223a36c90ff238b2ca
Mayank Nandwani <nandwanimayank007@gmail.com>
2024-03-19 16:37:39 +0530

    Fix typo in import statement and handle coverImage file upload conditionally

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,7 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import {ApiError} from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
-import {uploadOnCloudinary} from "../utils/cloudinary.js";
+import {uploadOnCloudinary} from "../utils/coludnary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler( async (req,res)=>{
@@ -23,7 +23,7 @@ const registerUser = asyncHandler( async (req,res)=>{
     ) {
         throw new ApiError(400, "All fields are required");
     }
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{ username }, { email }]
     })
     if(existedUser){
@@ -31,7 +31,11 @@ const registerUser = asyncHandler( async (req,res)=>{
     }
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImagePath = req.files?.coverImage[0]?.path;
+    // const coverImagePath = req.files?.coverImage[0]?.path;
+    let coverImagePath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length>0) {
+        coverImagePath = req.files.coverImage[0].path
+    } 
 
     if(!avatarLocalPath){
         throw new ApiError(404, "Avatar file is requrired");
--- src/utils/coludnary.js
+++ src/utils/coludnary.js
@@ -16,7 +16,8 @@ cloudinary.config({
             resource_type: "auto"
         })
         // file has been uploaded successfully
-        console.log("file is uploaded on cloudinary", response.url);
+        // console.log("file is uploaded on cloudinary", response.url);
+        fs.unlinkSync(localFIlePath)  //remove the locally saved temp file as the operation got failed
         return response;
         
     } catch (error) {
